b089135a54271d49a90755db88c3e8f9d339eb0c
TEDWEBB <twebby152@gmail.com>
2016-08-17 10:22:42 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Rhino", "Lemur", "Hippo", "Sloth", "Dogman", "Moonfish"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var programmers = ["Fruits", "Steak", "Socks", "Vegies", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
